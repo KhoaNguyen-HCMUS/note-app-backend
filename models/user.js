6ba1ce2d4eae0ad23bbe3c8f7c6e800efd1708ac
KhoaNguyen <anhkhoa515.dev@gmail.com>
2025-05-15 18:01:48 +0700

    feat: ✨ rm constraint on username

--- models/user.js
+++ models/user.js
@@ -5,12 +5,12 @@ const userSchema = new mongoose.Schema(
     username: {
       type: String,
       required: true,
-      unique: true,
       minlength: 3,
     },
     email: {
       type: String,
       required: true,
+      unique: true,
       match: /.+\@.+\..+/,
     },
     password: {
